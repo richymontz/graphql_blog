db6ed3247d421efa65e89c55f711d13b7cef6ade
Your Name <ingricardomontes@gmail.com>
2023-09-14 10:57:40 -0500

    add specs for register mutation

--- spec/factories/accounts.rb
+++ spec/factories/accounts.rb
@@ -4,5 +4,17 @@ FactoryBot.define do
     last_name { Faker::Name.last_name }
     email { Faker::Internet.email }
     password { Faker::Internet.password }
+    zipcode { Faker::Address.zip_code }
+    address { Faker::Address.street_address }
+   
+
+    trait :as_individual do
+      date_of_birth { '24-10-1966' }
+      account_role { 'individual' }
+    end
+
+    trait :blank_password do
+      password { nil }
+    end
   end
 end
---
+++ spec/graphql/mutations/register_spec.rb
@@ -0,0 +1,65 @@
+require 'rails_helper'
+
+RSpec.describe 'Mutation Register' do
+  context 'When is resquested with correct params' do
+    let(:account) { FactoryBot.build(:account, :as_individual) }
+    let(:query_response) { execute(account) }
+
+    it 'creates an account' do
+      expect(query_response.dig("data", "register", "email")).to be_present
+      expect(query_response.dig("data", "register", "token")).to be_present
+    end
+  end
+
+  context 'When wrong praams' do
+    let(:account) { FactoryBot.build(:account, :blank_password) }
+    let(:query_response) { execute(account) }
+
+    it 'gets register error' do
+      expect(query_response.dig("errors")).to be_present
+    end
+  end
+  
+  def execute(account)
+    GraphqlBlogSchema.execute(query_register, variables: { 
+      accountRole: account.account_role,
+      firstName: account.first_name,
+      lastName: account.last_name,
+      zipcode: account.zipcode,
+      address: account.address,
+      email: account.email, 
+      password: account.password,
+      dateOfBirth: '24-10-1966' 
+    }) 
+  end
+
+  def query_register
+    <<-GQL
+          mutation Register(
+            $accountRole: AccountRole!,
+            $firstName: String!,
+            $lastName:  String!,
+            $zipcode:  String!,
+            $address:  String!,
+            $email: String!,
+            $password: String!,
+            $dateOfBirth: String
+          ){
+            register(
+              accountRole: $accountRole,
+              firstName: $firstName,
+              lastName: $lastName,
+              zipcode: $zipcode,
+              address: $address,
+              email: $email,
+              password: $password,
+              dateOfBirth: $dateOfBirth
+            ){
+              email
+              token
+            }
+          }
+        
+      GQL
+  end
+end
